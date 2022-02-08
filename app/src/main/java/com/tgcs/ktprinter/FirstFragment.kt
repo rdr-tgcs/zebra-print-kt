dba789376c676dd13df1e3000802ea66e598b3ee
Bart Crispeels <bart.crispeels@toshibagcs.com>
2022-02-08 16:58:01 +0100

    Made BT MAC address configurable

--- app/src/main/java/com/tgcs/ktprinter/FirstFragment.kt
+++ app/src/main/java/com/tgcs/ktprinter/FirstFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.tgcs.ktprinter.databinding.FragmentFirstBinding
 import kotlinx.coroutines.*
 import java.lang.Exception
@@ -14,7 +17,7 @@ import java.lang.Exception
  */
 class FirstFragment : Fragment() {
 
-    private val printer: EcpPrinter
+    private lateinit var printer: EcpPrinter
     private val scope = CoroutineScope(SupervisorJob())
     private var _binding: FragmentFirstBinding? = null
 
@@ -33,20 +36,33 @@ class FirstFragment : Fragment() {
     }
 
     init {
+        /*
         val map = mapOf(
             "deviceType" to "zebra",
             "connectionType" to "bt",
-            "mac" to "48:A4:93:62:20:AF"
+            "mac" to "48:A4:93:6D:4E:09"
         )
 
         val factory = EcpPrinterFactory()
         printer = factory.create(map)
+        */
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val scope = CoroutineScope(SupervisorJob())
         binding.buttonFirst.setOnClickListener {
+
+            val txtMac = view.findViewById<TextInputEditText>(R.id.txtMACInput)
+
+            val map = mapOf(
+                "deviceType" to "zebra",
+                "connectionType" to "bt",
+                "mac" to txtMac.text.toString()
+            )
+
+            val factory = EcpPrinterFactory()
+            printer = factory.create(map)
 
             scope.launch {
                 for (i in 1..1000) {
